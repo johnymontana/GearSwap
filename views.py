04ef3b4ffe98103d5054affb6c24c33ea3bc91a7
johnymontana <lyonwj@gmail.com>
2013-01-19 16:15:26 -0700

    add index view

--- views.py
+++ views.py
@@ -1,11 +1,11 @@
 # Create your views here.
 from django.http import HttpResponse
-from gearswap.models import GearItem
+from GearSwap.models import GearItem
 from django.shortcuts import render_to_response
 from django.template import Context, loader, RequestContext
 
 def index(request):
-    return render_to_response('gearswap/index.html', {'title': 'Gear Surf'}, context_instance=RequestContext(request))
+    return render_to_response('gearswap/index.html', {'title': 'Gear Surf', 'subtitle':'Rent your gear', 'top_text':'How does it work', 'second_text':'We facilitate peer to peer gear rentals. Want gear? Rent it. Have gear? Rent it!'}, context_instance=RequestContext(request))
 
 #def list_all_gear(request):
 
